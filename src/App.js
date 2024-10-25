b353928b274b32691bfd4aafef2ed70fbb4b3b9e
luminawebs <luminawebs@gmail.com>
2024-10-24 21:00:18 -0500

    installed npm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import LandingPage from './pages/LandingPage';
+// import NewsPage from './pages/NewsPage';
+// import OrderList from './pages/OrderList';
+// import OrderForm from './pages/OrderForm';
 
 function App() {
   return (
+    
     <div className="App">
+         {/* <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/orders" element={<OrderList />} />
+        <Route path="/order/new" element={<OrderForm />} />
+      </Routes>
+    </Router> */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,11 +30,16 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reacts
         </a>
       </header>
     </div>
   );
 }
+
+
+
+
+
 
 export default App;
